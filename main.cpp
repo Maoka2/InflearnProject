0fc04ae5f96d91bf925125de737b426a7096a9e8
Maoka2 <a24358232@gmail.com>
2022-12-18 15:28:49 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    cout << "Hello, World!" << endl;
+    for(int i = 0; i < 10 ; i++){
+        cout << i << i * 2 << endl;
+    }
+    return 0;
+}
